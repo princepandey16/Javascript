4fb23c3fc77ed570f386a96af14637cc8b2b7d80
princepandey16 <133244254+princepandey16@users.noreply.github.com>
2024-02-25 20:11:05 +0000

    container config done3

---
+++ Lecture2./variable.js
@@ -0,0 +1,7 @@
+const accountId = 144458
+let accountName = "prince"
+var accountEmail = "xyz.email.com"
+
+let accountType;
+console.log(accountId)
+console.table([accountId,accountName,accountEmail,accountType])
