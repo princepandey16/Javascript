c92bb859f68caf9c811ca31f0b846dd1afc07df3
princepandey16 <133244254+princepandey16@users.noreply.github.com>
2024-02-27 18:33:05 +0000

    Conversion Done

---
+++ Lecture4/conversion.js
@@ -0,0 +1,25 @@
+let number1 = "33abc"
+
+console.log(typeof number1)
+
+let convertNumber = Number(number1)
+
+console.log(typeof convertNumber)
+console.log(convertNumber)
+
+let value = null
+let value1 = Number(value)
+console.log(typeof value1)
+console.log(value1)
+
+let c1 = undefined
+
+let c2 = String(c1)
+
+console.log(c2)
+console.log(typeof c2)
+
+// conversions
+// *Boolean *String *Number
+// Boolean of empty string gives false 
+// Boolean of string gives True
