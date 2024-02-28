3f4f17835ce0380d4565ee946e345e4af9141fac
princepandey16 <133244254+princepandey16@users.noreply.github.com>
2024-02-28 15:58:32 +0000

    comparision_between _DataType

---
+++ Lecture4/comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>2)
+// console.log(2<2)
+// console.log(2 ==2)
+
+console.log("2" >2)
+console.log("02">= 2)
+console.log(null>0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// jab bhi comaprison operator ka use karte hai to ye do tarike se kaam karta hai phela ye ki ye == ko number me convert nhi karta 
+// hai aur >= <= < > ko number me convert kar deta hai
+
+console.log(undefined>0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
