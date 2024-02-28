11a583a58824f46be8108baebd648ef4036ae10b
princepandey16 <133244254+princepandey16@users.noreply.github.com>
2024-02-28 15:41:06 +0000

    Operation_Conversion_done

---
+++ Lecture4/conversion2.js
@@ -0,0 +1,22 @@
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(2+2+"1")
+
+
+console.log("hii" + " Prince")
+console.log("hii" - " P")
+
+console.log(true)
+console.log(+true)
+
+console.log(+"")
