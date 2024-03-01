c48ad9de24b431adf300193821328cdb25b4d6db
princepandey16 <133244254+princepandey16@users.noreply.github.com>
2024-03-01 17:12:14 +0000

    String basics  Done

---
+++ Lecture5/strings.js
@@ -0,0 +1,28 @@
+const fullName = "Prince"
+const countRepo = 50;
+
+console.log(`hii i m ${fullName} and no of reop ${countRepo}`)
+
+const Name = new String('prince')
+console.log(Name)
+
+const gameName = "freeFire"
+console.log(gameName[2])
+console.log(gameName.__proto__)
+
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('r'))
+console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+console.log(gameName.slice(-4,4))
+
+
+const newStringOne = '      Prince   '
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+
