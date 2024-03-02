a4718092b5ed1af6748d210e9c4b6b9bf621d2f3
princepandey16 <133244254+princepandey16@users.noreply.github.com>
2024-03-02 04:48:09 +0000

    Dates and Times done

---
+++ Lecture5/date.js
@@ -0,0 +1,27 @@
+// let myDate = new Date()
+
+// console.log(myDate)
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.getMonth())
+// console.log(myDate.toISOString())
+
+
+// let myCreateDate = new Date(2024,0,23)
+// console.log(myCreateDate.toLocaleDateString())
+
+// let myCreatedate = new Date("01-14-2023")
+// console.log(myCreatedate)
+
+// let createMyDate = new Date("2024-02-12")
+// console.log(createMyDate)
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+
+
+new Date.toLocaleString('default',{
+    weekday:'long'
+})
+
